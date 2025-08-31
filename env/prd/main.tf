d7a40e466c1dedb9cb7553b212be5911ce8bf908
Cleiton Corrêa <cleitoncorreas@gmail.com>
2025-08-31 02:03:56 -0300

    Adiciona Output

--- env/prd/main.tf
+++ env/prd/main.tf
@@ -1,11 +1,11 @@
-module "awd_dev" {
+module "awd_prd" {
   source = "../../infra"
   instance_type = "t3.micro"
-  key_ssh = "IaC-DEV"
+  key_ssh = "IaC-PRD"
   ami_id = "ami-0360c520857e3138f"
   region_aws = "us-east-1"
 }
 
-output "public_ip_dev" {
+output "public_ip_prd" {
   value = module.awd_dev.public_ip
 }
